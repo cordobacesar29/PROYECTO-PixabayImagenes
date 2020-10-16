fb0499a47b2b02ae5879335cf0e74f18502a23fe
Cesar Cordoba <cordobacesar29@gmail.com>
2020-10-16 13:36:15 -0300

    agregando asyncAwait al proyecto

--- public/js/app.js
+++ public/js/app.js
@@ -46,21 +46,21 @@ function mostrarAlerta(mensaje) {
     }
 }
 
-function buscarImagenes() {
+ async function buscarImagenes() {
 
     const termino = document.querySelector('#termino').value;
 
     const key = '18691899-4401beccd0942ed1b2b16fb94';
     const url = `https://pixabay.com/api/?key=${key}&q=${termino}&per_page=${registrosPorPagina}&page=${paginaActual}`;
-
-    fetch(url)
-        .then( respuesta => respuesta.json() )
-        .then( resultado => {
-
-            totalPaginas = calcularPaginas(resultado.totalHits);
-
-            mostrarImagenes(resultado.hits);
-        })    
+        
+    try {
+        const respuesta = await fetch(url);
+        const resultado = await respuesta.json();
+        totalPaginas = calcularPaginas(resultado.totalHits);
+        mostrarImagenes(resultado.hits);
+    } catch (error) {
+        console.log(error);
+    }
 }
 
 // generador que registra la cantidad de elementos de acuerdo a las pagians
